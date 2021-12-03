e623101107ba5e994f7430e8a296cda91be7bf5b
Justin Lee <91635048+555960cuhsd@users.noreply.github.com>
2021-12-03 13:55:27 -0800

    Add files via upload

--- taco_clicker.py
+++ taco_clicker.py
@@ -1,5 +1,6 @@
 import random as rand
 import turtle as trtl
+import math
 import time
 
 ##################
@@ -9,20 +10,50 @@ import time
 wn = trtl.Screen()
 wn.addshape("taco.gif")
 wn.addshape("burrito.gif")
+wn.addshape("tabasco.gif")
+wn.addshape("fork.gif")
 
+# Taco
+taco = trtl.Turtle()
+score = 0
+
+taco.speed(0)
+taco.penup() 
+
+#-----game start-----
+game_start = False
+start_button = trtl.Turtle()
+start_button.shapesize(4)
+
+def start_game(x, y):
+  global game_start
+  game_start = True
+  start_button.hideturtle()
+  taco.showturtle()
+  taco.shape("taco.gif")
+start_button.onclick(start_game)
+
+while game_start != True:
+  taco.hideturtle()
+
+# Score Writer
 score_writer = trtl.Turtle()
 score_writer.speed(0)
 score_writer.penup()
 score_writer.hideturtle()
 score_writer.setposition(-200, -180)
 score_writer.pendown()
+# Font
+font_setup = ("Arial", "15", "normal")
 
-# Upgrade Button
-button = trtl.Turtle()
-button.speed(0)
-button.shape("burrito.gif")
-button.penup()
-button.setposition(-170, 180)
+score_writer.write(str(score) + " tacos", font = font_setup)
+
+# Taco per click upgrade Button
+tpc = trtl.Turtle()
+tpc.speed(0)
+tpc.shape("fork.gif")
+tpc.penup()
+tpc.setposition(-170, 180)
 
 # Score rate
 score_rate = 1
@@ -44,25 +75,24 @@ next.setposition(-220, 200)
 next.pendown()
 next.write("Next upgrade: " + str(upgrade_cost) + " tacos")
 
-# Chicken
-taco = trtl.Turtle()
-score = 0
-taco.shape("taco.gif")
-
-taco.speed(0)
-taco.penup() 
-
-font_setup = ("Arial", "15", "normal")
-
-score_writer.write(str(score) + " tacos", font = font_setup)
-
 # Point Text
 p = trtl.Turtle()
 p.hideturtle()
 p.speed(3)
 
+#-----autoclickers-----
 
+# tabasco
+tabasco = trtl.Turtle()
 
+sauce_list = [tabasco]
+tabasco_delay = 1000
+wait = False
+for sauces in sauce_list:
+  sauces.penup()
+  sauces.speed(0)
+tabasco.setposition(0, -200)
+tabasco.shape("tabasco.gif")
 ######################
 ### Game functions ###
 ######################
@@ -78,15 +108,12 @@ def update_score():
   score_writer.write(str(score) + " tacos", font = font_setup)
   
 
+def tpc_click(x, y):
+  upgrade(tpc) 
 
 
-def button_click(x, y):
-  upgrade() 
-
-def upgrade():
-  global score_rate
-  global score
-  global upgrade_cost
+def upgrade(upgrade):
+  global score_rate, score, upgrade_cost
   if score >= upgrade_cost:
     score_rate += 1
     rate.clear()
@@ -99,7 +126,43 @@ def upgrade():
     next.clear()
     next.write("Next upgrade: " + str(upgrade_cost) + " tacos")
 
-button.onclick(button_click)
+tpc.onclick(tpc_click)
 taco.onclick(taco_click)
 
+#-----autoclickers------
+
+# tabasco
+tabasco_click = 0
+
+def tabasco_clickk():
+  global score, wait
+  score += tabasco_click
+  wait = False
+
+def tabasco_autoclick():
+  global wait
+  if wait == False:
+    wait = True
+    wn.ontimer(tabasco_clickk, tabasco_delay)
+
+wn.ontimer(tabasco_clickk, tabasco_delay)
+
+tabasco_bought = False
+def buy_tabasco(x, y):
+  global tabasco_bought
+  tabasco_bought = True
+  buy_sauce()
+
+# all clickers
+while game_start == True:
+  tabasco_autoclick()
+  
+
+def buy_sauce():
+  global tabasco_click, tabasco_bought
+  if tabasco_bought == True:
+    tabasco_click += 1
+    tabasco_bought = False
+tabasco.onclick(buy_tabasco, "1")
+wn.listen()
 wn.mainloop()
